4f8d71f6127a4836e183136dd864c8897f4304ef
Filipe Medeiros <filipemedeiros@tuta.io>
2020-01-14 15:52:25 -0300

    Create function sort to (sort) the score, and create a rank  in string

--- src/commands/shipping.js
+++ src/commands/shipping.js
@@ -1,45 +1,76 @@
 const moment = require('moment');
 moment.locale('pt')
-
 const state = require('../bot/state.js')
 
+function shipping() {
 
-function command() {
-    
     const content = state.load();
     verifyHourOfShipping(content.lastShippingDate);
     chooseCouple(content);
     state.save(content);
 
 
-    function verifyHourOfShipping (lastShippingDate ){
-        return Math.round(new Date().getTime()/1000) - lastShippingDate >= 86400;
+    function verifyHourOfShipping(lastShippingDate) {
+        return Math.round(new Date().getTime() / 1000) - lastShippingDate >= 86400;
     }
 
-    function chooseCouple(content){
-        if (verifyHourOfShipping(content.lastShippingDate)){
-            firstPair = content.people[Math.floor(Math.random()*content.people.length)];
-            secondPair = content.people[Math.floor(Math.random()*content.people.length)];
-            
-            while (secondPair == firstPair) { 
-                secondPair = content.people[Math.floor(Math.random()*content.people.length)];
+    function chooseCouple(content) {
+        if (verifyHourOfShipping(content.lastShippingDate)) {
+            firstPair = content.people[Math.floor(Math.random() * content.people.length)];
+            secondPair = content.people[Math.floor(Math.random() * content.people.length)];
+
+            while (secondPair == firstPair) {
+                secondPair = content.people[Math.floor(Math.random() * content.people.length)];
+
             }
 
-            content.lastShippingCouple = firstPair + " + " + secondPair;
+            content.lastShippingCouple = firstPair.username + " + " + secondPair.username;
+            firstPair.score++
+            secondPair.score++
             content.nextTimeToChoose = moment().add(1, 'days').calendar();
-            content.lastShippingDate = Math.round(new Date().getTime()/1000);
-            
-            return "Casal do dia foi escolhido: " + content.lastShippingCouple + 
-            "\n" + "Novo casal do dia pode ser escolhido " + content.nextTimeToChoose;
+            content.lastShippingDate = Math.round(new Date().getTime() / 1000);
+
+
+
+            return "Casal do dia foi escolhido: " + content.lastShippingCouple +
+                "\n" + "Novo casal do dia pode ser escolhido " + content.nextTimeToChoose;
         }
 
-        else{
-            return "Ainda não pode ser escolher ainda! o anterior foi " + content.lastShippingCouple + 
-            "\n" + "Próximo casal pode ser escolhido " + content.nextTimeToChoose;
+        else {
+            return "Ainda não pode ser escolher ainda! o anterior foi " + content.lastShippingCouple +
+                "\n" + "Próximo casal pode ser escolhido " + content.nextTimeToChoose;
         }
+
+    }
+
+
+}
+
+function ranking() {
+
+    const content = state.load();
+    sort(content.people);
+    top(content)
+    state.save(content);
+
+    function sort(people) {
+        people.sort((a, b) => b.score - a.score);
     }
 }
 
+function top(content){
+    content.rank = "Top Lovers \n"
 
-command()
-module.exports = command
+        for (let index = 0; index < content.people.length; index++) {
+            content.rank += ((index+1 + ". " + (element = content.people[index].username) + " — " + 
+            (element = content.people[index].score) + "\n"));
+        }
+    }
+
+    ranking()
+    shipping()
+
+    module.exports = {
+        shipping,
+        ranking
+    }
