b38b2d37e5bdd84cf888abb8637db79dd76cf081
Filipe Medeiros <filipemedeiros@tuta.io>
2020-01-14 16:35:57 -0300

    init a simple bot

--- src/bot/init.js
+++ src/bot/init.js
@@ -1,5 +1,18 @@
-function empty () { 
-	return "Empty Body"
+const Telegraf = require('telegraf')
+const telegramApiKey = require(process.cwd() + '/src/credentials/telegram.json').apiKey
+
+
+function init(){
+const bot = new Telegraf(telegramApiKey)
+bot.start((ctx) => ctx.reply('Welcome!'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch()
 }
-   
-module.exports = empty
+
+
+init()
+
+
+module.exports = init;
