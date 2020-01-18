4c0decdc606f75017868b525dce3a21c48c075d3
FIlipe Medeiros <filipemedeiros@tuta.io>
2020-01-17 21:56:55 -0300

    Comunicacao com MongoDB

---
+++ bot/state.js
@@ -0,0 +1,30 @@
+const MongoClient = require('mongodb').MongoClient;
+
+const db_uri = process.env.DB_URI;
+
+let cachedDB = null;
+
+// TODO: This can probably be refactored
+const connectToDB = async () => {
+    if (cachedDB) return cachedDB;
+    const client = await MongoClient.connect(db_uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    const db = await client.db(process.env.DB_NAME);;
+    cachedDB = db;
+    return db;
+};
+
+async function peopleDb() {
+    const db = await connectToDB();
+    return await db.collection('people');
+}
+
+async function shippingDb() {
+    const db = await connectToDB();
+    return await db.collection('shipping');
+}
+
+
+module.exports = {
+    peopleDb,
+    shippingDb
+}
